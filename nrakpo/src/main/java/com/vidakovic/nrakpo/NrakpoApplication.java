3c63b1535439959d4ee9d6f19725c114b158623f
ezviddo <dominik.vidakovic@ericsson.com>
2019-07-29 14:03:56 +0200

    Fresh spring boot project

---
+++ nrakpo/src/main/java/com/vidakovic/nrakpo/NrakpoApplication.java
@@ -0,0 +1,13 @@
+package com.vidakovic.nrakpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NrakpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NrakpoApplication.class, args);
+	}
+
+}
---
+++ nrakpo/src/test/java/com/vidakovic/nrakpo/NrakpoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vidakovic.nrakpo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NrakpoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
