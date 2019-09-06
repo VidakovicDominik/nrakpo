fcfb51a86fe0da789330e7a577fda5dcf512cc53
ezviddo <dominik.vidakovic@ericsson.com>
2019-09-06 16:34:47 +0200

    front

---
+++ nrakpo/src/main/java/com/vidakovic/nrakpo/controller/PhotoController.java
@@ -0,0 +1,44 @@
+package com.vidakovic.nrakpo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <p>
+ * <b>Title: PhotoController  </b>
+ * </p>
+ * <p>
+ * <b> Description:
+ *
+ *
+ * </b>
+ * </p>
+ * <p>
+ * <b>Copyright:</b> Copyright (c) ETK 2019
+ * </p>
+ * <p>
+ * <b>Company:</b> Ericsson Nikola Tesla d.d.
+ * </p>
+ *
+ * @author ezviddo
+ * @version PA1
+ * <p>
+ * <b>Version History:</b>
+ * </p>
+ * <br>
+ * PA1 04-Sep-19
+ * @since 04-Sep-19 16:10:42
+ */
+
+@Controller
+@RequestMapping("/photo")
+public class PhotoController {
+
+    @GetMapping("/{id}")
+    public String showIndex(Model model) {
+        return "photo_details";
+    }
+}
+
